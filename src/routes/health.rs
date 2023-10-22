b378741bb350de626154aef304aba6850e4726e6
Chloe Pratas <chloevision97@gmail.com>
2023-10-23 00:27:34 +0200

    eww

--- src/routes/health.rs
+++ src/routes/health.rs
@@ -1,7 +1,7 @@
 use actix_web::{ get, HttpResponse };
 
 #[tracing::instrument]
-#[get("/health-check/")]
+#[get("/health-check")]
 pub async fn health_check() -> HttpResponse {
     tracing::event!(target: "backend", tracing::Level::DEBUG, "Accessing health-check endpoint.");
     HttpResponse::Ok().json("Application is safe and healthy.")
