ea10536521c4613fa5c39f0beb97714da9e53c88
Chloe Pratas <chloevision97@gmail.com>
2023-10-29 16:53:11 +0100

    cleaning some files for readability

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,15 @@ use dotenv::dotenv;
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     dotenv().ok();
+
     let settings = settings::get_settings().expect("Failed to load settings.");
+
     let subscriber = telemetry::get_subscriber(settings.clone().debug);
     telemetry::init_subscriber(subscriber);
+
     let application = Application::build(settings, None).await?;
     tracing::event!(target:"discord_backend", tracing::Level::INFO, "Listening on http://127.0.0.1:{}/", application.port());
     application.run_until_stopped().await?;
+
     Ok(())
 }
