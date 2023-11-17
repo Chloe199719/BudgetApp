7a31d3a7df88da66e32142c32fc17e6d87b5005a
Chloe Pratas <chloevision97@gmail.com>
2023-11-17 23:18:48 +0100

    Adding first part pw reset

--- src/routes/users/mod.rs
+++ src/routes/users/mod.rs
@@ -1,21 +1,21 @@
-pub mod register;
 pub mod confirm_registration;
-pub mod login;
-pub mod logout;
-mod update_user;
 mod current_user;
 mod generate_new_token;
+pub mod login;
+pub mod logout;
+mod password_change;
+pub mod register;
+mod update_user;
 
 pub fn auth_routes_config(cfg: &mut actix_web::web::ServiceConfig) {
     cfg.service(
-        actix_web::web
-            ::scope("/users")
+        actix_web::web::scope("/users")
             .service(register::register_user)
             .service(confirm_registration::config)
             .service(login::login_user)
             .service(logout::logout_user)
             .service(update_user::update_users_details)
             .service(current_user::get_current_user)
-            .service(generate_new_token::regenerate_token)
+            .service(generate_new_token::regenerate_token),
     );
 }
