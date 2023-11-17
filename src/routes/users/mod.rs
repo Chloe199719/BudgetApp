85e6dc0e4f5c465aa7327395845addc54c4cdc5f
Chloe Pratas <chloevision97@gmail.com>
2023-11-18 00:13:06 +0100

    Add password change functionality to user routes

--- src/routes/users/mod.rs
+++ src/routes/users/mod.rs
@@ -16,6 +16,9 @@ pub fn auth_routes_config(cfg: &mut actix_web::web::ServiceConfig) {
             .service(logout::logout_user)
             .service(update_user::update_users_details)
             .service(current_user::get_current_user)
-            .service(generate_new_token::regenerate_token),
+            .service(generate_new_token::regenerate_token)
+            .service(password_change::request_change::request_password_change)
+            .service(password_change::confirm_change_request::confirm_change_password_token)
+            .service(password_change::change::change_user_password),
     );
 }
