3215b4589b1e55ff15a4236f87ff5ef8e236f06a
Chloe Pratas <chloevision97@gmail.com>
2023-11-19 14:48:59 +0100

    Add login success test case

--- tests/api/users/login.rs
+++ tests/api/users/login.rs
@@ -1,4 +1,4 @@
-use discord_backend::types::general::ErrorResponse;
+use discord_backend::types::{general::ErrorResponse, UserVisible};
 use fake::{ faker::{ name::en::NameWithTitle, internet::en::SafeEmail }, Fake };
 use serde::{ Serialize, Deserialize };
 use sqlx::PgPool;
@@ -52,4 +52,34 @@ async fn test_login_user_failure_not_found(pool: PgPool) {
         .expect("Failed to parse error response");
 
     assert_eq!(error_response.error, "User with those details doesn't exist");
+}
+
+#[sqlx::test]
+async fn test_login_user_success(pool:PgPool) {
+    let app = spawn_app(pool.clone()).await;
+
+    let login_body = LoginUser {
+        email: app.test_user.email.clone(),
+        password: app.test_user.password.clone(),
+    };
+
+    let login_response = app.post_login(&login_body).await;
+
+    assert!(login_response.status().is_success());
+
+    //Check that there is a cookie present 
+
+    let headers = login_response.headers();
+    assert!(headers.get("set-cookie").is_some());
+
+    let cookie_str = headers.get("set-cookie").unwrap().to_str().unwrap();
+    assert!(cookie_str.contains("sessionid="));
+
+    // Check response 
+
+    let response = login_response.json::<UserVisible>().await.expect("Failed to parse response");
+
+    assert_eq!(response.email, app.test_user.email);
+    assert!(response.is_active);
+    assert_eq!(response.id, response.profile.user_id); 
 }
