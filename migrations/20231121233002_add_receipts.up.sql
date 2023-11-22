8a38891e3b54d1a624c075d1f63bee910a8bbaa8
Chloe Pratas <chloevision97@gmail.com>
2023-11-22 01:19:18 +0100

    Update receipts table and add unique constraints

--- migrations/20231121233002_add_receipts.up.sql
+++ migrations/20231121233002_add_receipts.up.sql
@@ -1,14 +1,14 @@
 -- Add up migration script here
 CREATE TABLE IF NOT EXISTS receipts (
     id SERIAL PRIMARY KEY,
-    transaction_id INT NOT NULL,
+    transaction_id INT UNIQUE NOT NULL,
     user_Id UUID NOT NULL,
     receipt_url VARCHAR(255) NOT NULL,
     created_at timestamptz NOT NULL DEFAULT NOW(),
     updated_at timestamptz NOT NULL DEFAULT NOW(),
     FOREIGN KEY (user_id) REFERENCES users(id),
-    CONSTRAINT transaction_id FOREIGN KEY (transaction_id) REFERENCES transactions(transaction_id)
+    FOREIGN KEY (transaction_id) REFERENCES transactions(transaction_id)
 );
 
-ALTER TABLE transactions ADD COLUMN IF NOT EXISTS receipt_id INT;
+ALTER TABLE transactions ADD COLUMN IF NOT EXISTS receipt_id INT UNIQUE;
 ALTER TABLE transactions ADD CONSTRAINT receipt_id FOREIGN KEY (receipt_id) REFERENCES receipts(id);
