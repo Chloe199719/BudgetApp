83f68ed1939480ac911ab2537123ed1ea99a4dd8
Chloe Pratas <chloevision97@gmail.com>
2023-11-25 19:51:54 +0100

    Add error handling for delete category API

--- tests/api/categories/delete_category.rs
+++ tests/api/categories/delete_category.rs
@@ -1,4 +1,4 @@
-use discord_backend::types::{ UserVisible, general::SuccessResponse };
+use discord_backend::types::{ UserVisible, general::{ SuccessResponse, ErrorResponse } };
 use sqlx::PgPool;
 
 use crate::{ users::login::LoginUser, helpers::spawn_app, categories::create_category_in_db };
@@ -38,4 +38,54 @@ async fn test_delete_category_success(pool: PgPool) {
         .expect("Failed to parse delete category response");
 
     assert_eq!(delete_category_response_body.message, "Successfully deleted category");
+}
+
+#[sqlx::test]
+async fn test_delete_category_error_not_logged_in(pool: PgPool) {
+    let app = spawn_app(pool.clone()).await;
+
+    let delete_category_response = app.api_client
+        .delete(&format!("{}/categories/delete/{}", app.address, 1))
+        .send().await
+        .expect("Failed to execute request.");
+
+    assert_eq!(delete_category_response.status().as_u16(), 401);
+
+    let delete_category_response_body = delete_category_response
+        .json::<ErrorResponse>().await
+        .expect("Failed to parse delete category response");
+
+    assert_eq!(
+        delete_category_response_body.error,
+        "You are not logged in. Kindly ensure you are logged in and try again"
+    );
+}
+
+#[sqlx::test]
+async fn test_delete_category_error_category_dosent_exist(pool: PgPool) {
+    let app = spawn_app(pool.clone()).await;
+
+    //Act - Part 1 - Login
+    let login_body = LoginUser {
+        email: app.test_user.email.clone(),
+        password: app.test_user.password.clone(),
+    };
+
+    let login_response = app.post_login(&login_body).await;
+
+    assert!(login_response.status().is_success());
+
+    //Act - Part 2 - Delete category
+    let delete_category_response = app.api_client
+        .delete(&format!("{}/categories/delete/{}", app.address, 1))
+        .send().await
+        .expect("Failed to execute request.");
+
+    assert!(delete_category_response.status().is_client_error());
+
+    let delete_category_response_body = delete_category_response
+        .json::<ErrorResponse>().await
+        .expect("Failed to parse delete category response");
+
+    assert_eq!(delete_category_response_body.error, "Category does not exist");
 }
