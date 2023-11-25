bdb4f6a4e0f2779ea182425d39ad394fcae9f37a
Chloe Pratas <chloevision97@gmail.com>
2023-11-25 16:06:51 +0100

    Add delete_category service to categories_routes_config

--- src/routes/categories/mod.rs
+++ src/routes/categories/mod.rs
@@ -3,5 +3,10 @@ pub mod delete_category;
 use actix_web::web::ServiceConfig;
 
 pub fn categories_routes_config(cfg: &mut ServiceConfig) {
-    cfg.service(actix_web::web::scope("/categories").service(create_category::create_category));
+    cfg.service(
+        actix_web::web
+            ::scope("/categories")
+            .service(create_category::create_category)
+            .service(delete_category::delete_category)
+    );
 }
