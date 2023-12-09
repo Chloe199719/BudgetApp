f70062185ad506fde9a35d0b512baaded8d3e50b
Chloe Pratas <chloevision97@gmail.com>
2023-12-09 22:37:15 +0100

    Fix import statement in get_category_by_id test

--- tests/api/categories/get_category_by_id.rs
+++ tests/api/categories/get_category_by_id.rs
@@ -1,6 +1,6 @@
+use budget_app::types::{categories::Category, UserVisible};
 use serde::{Deserialize, Serialize};
 use sqlx::PgPool;
-use BudgetApp::types::{categories::Category, UserVisible};
 
 use crate::{
     categories::{create_category_in_db, TEST_CATEGORY_DESCRIPTION, TEST_CATEGORY_NAME},
