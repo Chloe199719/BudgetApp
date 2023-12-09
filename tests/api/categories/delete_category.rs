fa3d68c65e0859fdbf49b299212094dd473aa1e9
Chloe Pratas <chloevision97@gmail.com>
2023-12-09 22:15:44 +0100

    Added test to check if default category cannot be deleted

--- tests/api/categories/delete_category.rs
+++ tests/api/categories/delete_category.rs
@@ -108,6 +108,41 @@ async fn test_delete_category_error_category_dosent_exist(pool: PgPool) {
 
     assert_eq!(
         delete_category_response_body.error,
-        "Category does not exist"
+        "Category does not exist."
+    );
+}
+
+#[sqlx::test]
+async fn test_delete_category_error_category_is_default(pool: PgPool) {
+    let app = spawn_app(pool.clone()).await;
+
+    //Act - Part 1 - Login
+    let login_body = LoginUser {
+        email: app.test_user.email.clone(),
+        password: app.test_user.password.clone(),
+    };
+
+    let login_response = app.post_login(&login_body).await;
+
+    assert!(login_response.status().is_success());
+
+    //Act - Part 2 - Delete category
+    let delete_category_response = app
+        .api_client
+        .delete(&format!("{}/categories/delete/{}", app.address, 1))
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    assert!(delete_category_response.status().is_client_error());
+
+    let delete_category_response_body = delete_category_response
+        .json::<ErrorResponse>()
+        .await
+        .expect("Failed to parse delete category response");
+
+    assert_eq!(
+        delete_category_response_body.error,
+        "You cannot delete your default category"
     );
 }
