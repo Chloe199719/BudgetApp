bc0c0d25c4bd87e226be7c2376a310067516d4e8
Chloe Pratas <chloevision97@gmail.com>
2023-12-10 22:42:43 +0100

    Add create_transaction endpoint and related structs

--- src/routes/transactions/create_transaction.rs
+++ src/routes/transactions/create_transaction.rs
@@ -1,4 +1,10 @@
 use actix_multipart::form;
+use actix_web::{post, web::Data, HttpResponse};
+use sqlx::PgPool;
+
+use crate::{
+    routes::users::logout::session_user_id, types::general::ErrorResponse, uploads::client::Client,
+};
 
 #[derive(form::MultipartForm)]
 pub struct CreateTransactionRequest {
@@ -32,4 +38,42 @@ impl std::fmt::Display for TransactionType {
             Self::WITHDRAWAL => write!(f, "WITHDRAWAL"),
         }
     }
+}
+#[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
+pub struct Transaction {
+    pub id: i32,
+    pub amount: i64,
+    pub category_id: Option<i32>,
+    pub description: String,
+    pub transaction_date: chrono::NaiveDate,
+    pub transaction_type: TransactionType,
+    pub receipt: Option<String>,
+    pub user_id: i32,
+}
+#[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
+pub struct Receipt {
+    pub user_id: uuid::Uuid,
+    pub transaction_id: i32,
+    pub url: String,
+}
+
+#[tracing::instrument(name = "Creating a transaction", skip(form, pool, session, s3_client))]
+#[post("/create")]
+pub async fn create_transaction(
+    pool: Data<PgPool>,
+    form: actix_multipart::form::MultipartForm<CreateTransactionRequest>,
+    session: actix_session::Session,
+    s3_client: Data<Client>,
+) -> HttpResponse {
+    let session_uuid = match session_user_id(&session).await {
+        Ok(id) => id,
+        Err(e) => {
+            tracing::event!(target: "session", tracing::Level::ERROR, "Failed to get user from session. User unauthorized: {}", e);
+            return actix_web::HttpResponse::Unauthorized().json(ErrorResponse {
+                error: "You are not logged in. Kindly ensure you are logged in and try again"
+                    .to_string(),
+            });
+        }
+    };
+    todo!("Implement this endpoint.")
 }
