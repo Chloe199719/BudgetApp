5984bf76a74a99de34725d8acd3f19e5bfbf8381
Chloe Pratas <chloevision97@gmail.com>
2023-12-16 23:44:51 +0100

    Adding error fails

--- tests/api/transactions/create_a_transaction.rs
+++ tests/api/transactions/create_a_transaction.rs
@@ -278,3 +278,156 @@ async fn test_create_transaction_success(pool: PgPool) {
         create_transaction_body.category_id
     );
 }
+
+#[sqlx::test]
+async fn test_create_transaction_success_not_logged_in(pool: PgPool) {
+    let app = spawn_app(pool.clone()).await;
+
+    //Act - Part 1 - Create transaction
+
+    let create_transaction_body = CreateTransactionWithCurrencyAndCategory {
+        transaction_date: Utc::now(),
+        transaction_type: TransactionType::DEPOSIT,
+        description: Sentence(1..2).fake(),
+        amount: 100.0,
+        currency: "USD".to_string(),
+        category_id: 1,
+    };
+
+    let create_transaction_response = app
+        .api_client
+        .post(&format!("{}/transactions/create", app.address))
+        .multipart(
+            Form::new()
+                .text(
+                    "transaction_date",
+                    create_transaction_body.transaction_date.to_rfc3339(),
+                )
+                .text(
+                    "transaction_type",
+                    create_transaction_body.transaction_type.to_string(),
+                )
+                .text("description", create_transaction_body.description.clone())
+                .text("amount", create_transaction_body.amount.to_string())
+                .text("currency", create_transaction_body.currency.clone())
+                .text(
+                    "category_id",
+                    create_transaction_body.category_id.to_string(),
+                ),
+        )
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    assert!(create_transaction_response.status().is_client_error());
+}
+#[sqlx::test]
+async fn test_create_transaction_error_missing_description(pool: PgPool) {
+    let app = spawn_app(pool.clone()).await;
+    //Act - Part 1 - Login
+    let login_body = LoginUser {
+        email: app.test_user.email.clone(),
+        password: app.test_user.password.clone(),
+    };
+
+    let login_response = app.post_login(&login_body).await;
+
+    assert!(login_response.status().is_success());
+
+    let _login_response_body = login_response
+        .json::<UserVisible>()
+        .await
+        .expect("Failed to parse login response");
+
+    //Act - Part 2 - Create transaction
+
+    let create_transaction_body = CreateTransactionWithCurrencyAndCategory {
+        transaction_date: Utc::now(),
+        transaction_type: TransactionType::DEPOSIT,
+        description: Sentence(1..2).fake(),
+        amount: 100.0,
+        currency: "USD".to_string(),
+        category_id: 1,
+    };
+
+    let create_transaction_response = app
+        .api_client
+        .post(&format!("{}/transactions/create", app.address))
+        .multipart(
+            Form::new()
+                .text(
+                    "transaction_date",
+                    create_transaction_body.transaction_date.to_rfc3339(),
+                )
+                .text(
+                    "transaction_type",
+                    create_transaction_body.transaction_type.to_string(),
+                )
+                .text("amount", create_transaction_body.amount.to_string())
+                .text("currency", create_transaction_body.currency.clone())
+                .text(
+                    "category_id",
+                    create_transaction_body.category_id.to_string(),
+                ),
+        )
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    assert!(create_transaction_response.status().is_client_error());
+}
+#[sqlx::test]
+async fn test_create_transaction_error_missing_amount(pool: PgPool) {
+    let app = spawn_app(pool.clone()).await;
+    //Act - Part 1 - Login
+    let login_body = LoginUser {
+        email: app.test_user.email.clone(),
+        password: app.test_user.password.clone(),
+    };
+
+    let login_response = app.post_login(&login_body).await;
+
+    assert!(login_response.status().is_success());
+
+    let _login_response_body = login_response
+        .json::<UserVisible>()
+        .await
+        .expect("Failed to parse login response");
+
+    //Act - Part 2 - Create transaction
+
+    let create_transaction_body = CreateTransactionWithCurrencyAndCategory {
+        transaction_date: Utc::now(),
+        transaction_type: TransactionType::DEPOSIT,
+        description: Sentence(1..2).fake(),
+        amount: 100.0,
+        currency: "USD".to_string(),
+        category_id: 1,
+    };
+
+    let create_transaction_response = app
+        .api_client
+        .post(&format!("{}/transactions/create", app.address))
+        .multipart(
+            Form::new()
+                .text(
+                    "transaction_date",
+                    create_transaction_body.transaction_date.to_rfc3339(),
+                )
+                .text(
+                    "transaction_type",
+                    create_transaction_body.transaction_type.to_string(),
+                )
+                .text("description", create_transaction_body.description.clone())
+                .text("currency", create_transaction_body.currency.clone())
+                .text(
+                    "category_id",
+                    create_transaction_body.category_id.to_string(),
+                ),
+        )
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    assert!(create_transaction_response.status().is_client_error());
+}
