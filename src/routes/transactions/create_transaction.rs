5ceefbabb9a831df38407daaeb42d6fcf7abad77
Chloe Pratas <chloevision97@gmail.com>
2023-12-16 15:26:15 +0100

    Finishing the create transaction option

--- src/routes/transactions/create_transaction.rs
+++ src/routes/transactions/create_transaction.rs
@@ -1,4 +1,6 @@
+use actix_multipart::form;
 use actix_web::{post, web::Data, HttpResponse};
+use chrono::Utc;
 
 use sqlx::PgPool;
 
@@ -7,12 +9,24 @@ use crate::{
     types::{
         general::ErrorResponse,
         transactions::create::{
-            CreateTransactionRequest, Transaction, TransactionCurrency, TransactionOutcome,
+            Transaction, TransactionCurrency, TransactionOutcome, TransactionOutcomeWithReceipt,
             TransactionType,
         },
     },
     uploads::client::Client,
 };
+
+#[derive(form::MultipartForm)]
+pub struct CreateTransactionRequest {
+    pub amount: form::text::Text<f64>,
+    pub category_id: Option<form::text::Text<i32>>,
+    pub description: form::text::Text<String>,
+    pub transaction_date: form::text::Text<chrono::DateTime<Utc>>,
+    pub transaction_type: form::text::Text<TransactionType>,
+    pub currency: Option<form::text::Text<TransactionCurrency>>,
+    #[multipart(limit = "1 MiB")]
+    pub receipt: Option<form::tempfile::TempFile>,
+}
 
 #[tracing::instrument(name = "Creating a transaction", skip(form, pool, session, s3_client))]
 #[post("/create")]
@@ -45,13 +59,126 @@ pub async fn create_transaction(
         }
     };
 
-    todo!("Implement this endpoint.")
+    let mut transaction_struct = Transaction {
+        amount: form.amount.0,
+        category_id: None,
+        description: form.description.0.clone(),
+        transaction_date: form.transaction_date.0,
+        receipt_id: None,
+        transaction_type: form.transaction_type.0.clone(),
+        currency: None,
+    };
+
+    if let Some(category_id) = &form.category_id {
+        transaction_struct.category_id = Some(category_id.0);
+    } else {
+        transaction_struct.category_id = match get_users_default_category(
+            &session_uuid,
+            &mut transaction,
+        )
+        .await
+        {
+            Ok(category_id) => Some(category_id),
+            Err(e) => {
+                tracing::event!(target: "backend", tracing::Level::ERROR, "Unable to get default category: {:#?}", e);
+                return actix_web::HttpResponse::InternalServerError().json(ErrorResponse {
+                    error: "Something unexpected happened. Kindly try again.".to_string(),
+                });
+            }
+        }
+    }
+
+    if let Some(currency) = &form.currency {
+        transaction_struct.currency = Some(currency.0.clone());
+    } else {
+        transaction_struct.currency = match get_users_default_currency(
+            &session_uuid,
+            &mut transaction,
+        )
+        .await
+        {
+            Ok(currency) => Some(currency),
+            Err(e) => {
+                tracing::event!(target: "backend", tracing::Level::ERROR, "Unable to get default currency: {:#?}", e);
+                return actix_web::HttpResponse::InternalServerError().json(ErrorResponse {
+                    error: "Something unexpected happened. Kindly try again.".to_string(),
+                });
+            }
+        }
+    }
+
+    let save_transaction = match save_transaction_without_recipe(
+        transaction_struct,
+        &session_uuid,
+        &mut transaction,
+    )
+    .await
+    {
+        Ok(transaction) => transaction,
+        Err(e) => {
+            transaction.rollback().await.unwrap();
+            tracing::event!(target: "backend", tracing::Level::ERROR, "Unable to save transaction: {:#?}", e);
+            return actix_web::HttpResponse::InternalServerError().json(ErrorResponse {
+                error: "Something unexpected happened. Kindly try again.".to_string(),
+            });
+        }
+    };
+
+    if let Some(recipe) = &form.0.receipt {
+        let s3_key_prefix = format!(
+            "receipts/{}/{}",
+            session_uuid, save_transaction.transaction_id
+        );
+        let upload_file = s3_client.upload(recipe, &s3_key_prefix).await;
+
+        match save_recipe_url(
+            save_transaction.transaction_id,
+            &upload_file.s3_url,
+            &session_uuid,
+            &mut transaction,
+        )
+        .await
+        {
+            Ok(transaction_outcome) => match transaction.commit().await {
+                Ok(_) => {
+                    tracing::event!(target: "backend", tracing::Level::INFO, "Transaction committed successfully");
+                    return actix_web::HttpResponse::Ok().json(transaction_outcome);
+                }
+                Err(e) => {
+                    tracing::event!(target: "backend", tracing::Level::ERROR, "Unable to commit transaction: {:#?}", e);
+                    return actix_web::HttpResponse::InternalServerError().json(ErrorResponse {
+                        error: "Something unexpected happened. Kindly try again.".to_string(),
+                    });
+                }
+            },
+            Err(e) => {
+                transaction.rollback().await.unwrap();
+                tracing::event!(target: "backend", tracing::Level::ERROR, "Unable to save recipe url: {:#?}", e);
+                return actix_web::HttpResponse::InternalServerError().json(ErrorResponse {
+                    error: "Something unexpected happened. Kindly try again.".to_string(),
+                });
+            }
+        }
+    }
+
+    match transaction.commit().await {
+        Ok(_) => {
+            tracing::event!(target: "backend", tracing::Level::INFO, "Transaction committed successfully");
+            HttpResponse::Ok().json(save_transaction)
+        }
+        Err(e) => {
+            tracing::event!(target: "backend", tracing::Level::ERROR, "Unable to commit transaction: {:#?}", e);
+            return actix_web::HttpResponse::InternalServerError().json(ErrorResponse {
+                error: "Something unexpected happened. Kindly try again.".to_string(),
+            });
+        }
+    }
 }
 
 #[tracing::instrument(name = "Save transaction in DB", skip(pool))]
 pub async fn save_transaction_without_recipe(
     transaction_data: Transaction,
-    user_id: uuid::Uuid,
+    user_id: &uuid::Uuid,
     pool: &mut sqlx::Transaction<'_, sqlx::Postgres>,
 ) -> Result<TransactionOutcome, sqlx::Error> {
     let transaction = sqlx::query_as!(
@@ -65,12 +192,102 @@ pub async fn save_transaction_without_recipe(
         &transaction_data.category_id.unwrap(),
         &transaction_data.description,
         &transaction_data.transaction_date,
-        &transaction_data.transaction_type as &TransactionType,
+        TransactionType::DEPOSIT as TransactionType,
         user_id,
-        transaction_data.currency as Option<TransactionCurrency>,
+        transaction_data.currency.unwrap() as TransactionCurrency,
     )
     .fetch_one(pool.as_mut())
     .await?;
     Ok(transaction)
-    // todo!("Implement this function.")
+}
+
+#[tracing::instrument(name = "Save recipe url in DB", skip(pool))]
+pub async fn save_recipe_url(
+    transaction_id: i32,
+    url: &str,
+    user_id: &uuid::Uuid,
+    pool: &mut sqlx::Transaction<'_, sqlx::Postgres>,
+) -> Result<TransactionOutcomeWithReceipt, sqlx::Error> {
+    let recipe = sqlx::query!(
+        r#"
+        INSERT INTO receipts (transaction_id, receipt_url, user_id)
+        VALUES ($1, $2, $3)
+        RETURNING id;
+        "#,
+        transaction_id,
+        url,
+        user_id,
+    )
+    .fetch_one(pool.as_mut())
+    .await?;
+
+    let transaction_update = sqlx::query_as!(
+        TransactionOutcome,
+        r#"
+        UPDATE transactions
+        SET receipt_id = $1
+        WHERE transaction_id = $2
+    
+        returning transaction_id, amount, category_id, description, date, transaction_type as "transaction_type: _", receipt_id, user_id , currency as "currency: _" ;
+        "#,
+        recipe.id,
+        transaction_id,
+    )
+    .fetch_one(pool.as_mut())
+    .await?;
+    let transaction_with_recipe = TransactionOutcomeWithReceipt {
+        transaction_id: transaction_update.transaction_id,
+        amount: transaction_update.amount,
+        category_id: transaction_update.category_id,
+        description: transaction_update.description,
+        date: transaction_update.date,
+        transaction_type: transaction_update.transaction_type,
+        receipt_id: transaction_update.receipt_id,
+        receipt_url: Some(url.to_string()),
+        user_id: transaction_update.user_id,
+        currency: transaction_update.currency,
+    };
+    Ok(transaction_with_recipe)
+}
+
+#[tracing::instrument(name = "Get usersDefault Category", skip(pool))]
+pub async fn get_users_default_category(
+    user_id: &uuid::Uuid,
+    pool: &mut sqlx::Transaction<'_, sqlx::Postgres>,
+) -> Result<i32, sqlx::Error> {
+    let category = sqlx::query!(
+        r#"
+        SELECT category_id
+        FROM categories
+        WHERE user_id = $1 AND is_default = true;
+        "#,
+        user_id,
+    )
+    .fetch_one(pool.as_mut())
+    .await?;
+    Ok(category.category_id)
+}
+
+#[tracing::instrument(name = "Get User Default Currency", skip(pool))]
+pub async fn get_users_default_currency(
+    user_id: &uuid::Uuid,
+    pool: &mut sqlx::Transaction<'_, sqlx::Postgres>,
+) -> Result<TransactionCurrency, sqlx::Error> {
+    let currency = sqlx::query_as!(
+        CurrencyReturn,
+        r#"
+        SELECT currency as "currency: _"
+        FROM user_profile
+        WHERE user_id = $1
+       
+        "#,
+        user_id,
+    )
+    .fetch_one(pool.as_mut())
+    .await?;
+    Ok(currency.currency)
+}
+
+pub struct CurrencyReturn {
+    pub currency: TransactionCurrency,
 }
