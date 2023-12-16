2ea4900fb1ab45a5a377f5f9a674a8d6f01e00c3
Chloe Pratas <chloevision97@gmail.com>
2023-12-16 15:25:52 +0100

    Ading Types

--- src/types/transactions/create.rs
+++ src/types/transactions/create.rs
@@ -1,39 +1,41 @@
 use actix_multipart::form;
 
 use chrono::Utc;
+use serde::{Deserialize, Serialize};
 
 #[derive(form::MultipartForm)]
 pub struct CreateTransactionRequest {
-    pub amount: form::text::Text<i64>,
+    pub amount: form::text::Text<f64>,
     pub category_id: Option<form::text::Text<i32>>,
     pub description: form::text::Text<String>,
-    pub transaction_date: form::text::Text<chrono::NaiveDate>,
+    pub transaction_date: form::text::Text<chrono::DateTime<Utc>>,
     pub transaction_type: form::text::Text<TransactionType>,
     pub currency: Option<form::text::Text<TransactionCurrency>>,
     #[multipart(limit = "1 MiB")]
     pub receipt: Option<form::tempfile::TempFile>,
 }
 #[derive(Debug, Clone, serde::Serialize, serde::Deserialize, sqlx::Type)]
-#[sqlx(type_name = "transaction_type", rename_all = "SCREAMING_SNAKE_CASE")]
+#[sqlx(type_name = "transaction_type")]
 pub enum TransactionType {
-    Deposit,
-    Withdrawal,
+    DEPOSIT,
+    WITHDRAWAL,
 }
 impl std::str::FromStr for TransactionType {
     type Err = String;
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         match s {
-            "DEPOSIT" => Ok(Self::Deposit),
-            "WITHDRAWAL" => Ok(Self::Withdrawal),
+            "DEPOSIT" => Ok(Self::DEPOSIT),
+            "WITHDRAWAL" => Ok(Self::WITHDRAWAL),
             _ => Err(format!("{} is not a valid transaction type.", s)),
         }
     }
 }
+
 impl std::fmt::Display for TransactionType {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Self::Deposit => write!(f, "DEPOSIT"),
-            Self::Withdrawal => write!(f, "WITHDRAWAL"),
+            Self::DEPOSIT => write!(f, "DEPOSIT"),
+            Self::WITHDRAWAL => write!(f, "WITHDRAWAL"),
         }
     }
 }
@@ -84,7 +86,21 @@ pub struct TransactionOutcome {
     pub currency: TransactionCurrency,
 }
 
-#[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
+#[derive(Debug, Clone, serde::Serialize, serde::Deserialize, sqlx::FromRow)]
+pub struct TransactionOutcomeWithReceipt {
+    pub transaction_id: i32,
+    pub amount: f64,
+    pub category_id: i32,
+    pub description: String,
+    pub date: chrono::DateTime<Utc>,
+    pub transaction_type: String,
+    pub receipt_id: Option<i32>,
+    pub receipt_url: Option<String>,
+    pub user_id: uuid::Uuid,
+    pub currency: TransactionCurrency,
+}
+
+#[derive(Debug, Clone, serde::Serialize, serde::Deserialize, sqlx::FromRow)]
 pub struct Transaction {
     pub amount: f64,
     pub category_id: Option<i32>,
@@ -99,4 +115,14 @@ pub struct Receipt {
     pub user_id: uuid::Uuid,
     pub transaction_id: i32,
     pub url: String,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct ReturnRecipe {
+    pub transaction_id: i32,
+    pub id: i32,
+    pub receipt_url: String,
+    pub user_id: uuid::Uuid,
+    pub created_at: chrono::DateTime<Utc>,
+    pub updated_at: chrono::DateTime<Utc>,
 }
