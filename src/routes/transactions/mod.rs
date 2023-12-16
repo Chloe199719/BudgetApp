4830b218809741b94649b3a862edfc68fbbf5f83
Chloe Pratas <chloevision97@gmail.com>
2023-12-16 15:26:00 +0100

    Adding the route

--- src/routes/transactions/mod.rs
+++ src/routes/transactions/mod.rs
@@ -3,5 +3,7 @@ pub mod create_transaction;
 use actix_web::web::ServiceConfig;
 
 pub fn transactions_routes_config(cfg: &mut ServiceConfig) {
-    cfg.service(actix_web::web::scope("/transactions"));
+    cfg.service(
+        actix_web::web::scope("/transactions").service(create_transaction::create_transaction),
+    );
 }
