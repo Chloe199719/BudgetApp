5d5a882909d8ab21b05a574f55cc4dbecc51bdd7
Chloe Pratas <chloevision97@gmail.com>
2023-12-17 19:38:53 +0100

    get_all_transactions_by_category

--- src/routes/transactions/mod.rs
+++ src/routes/transactions/mod.rs
@@ -1,12 +1,13 @@
 pub mod create_transaction;
+pub mod get_all_transactions_by_categorie;
 pub mod get_all_transactions_by_user;
-
 use actix_web::web::ServiceConfig;
 
 pub fn transactions_routes_config(cfg: &mut ServiceConfig) {
     cfg.service(
         actix_web::web::scope("/transactions")
             .service(create_transaction::create_transaction)
-            .service(get_all_transactions_by_user::get_all_transactions_by_user),
+            .service(get_all_transactions_by_user::get_all_transactions_by_user)
+            .service(get_all_transactions_by_categorie::get_all_transactions_by_category),
     );
 }
