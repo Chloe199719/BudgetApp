d169a029161862589f595f747fab3f0c76d269e2
Chloe Pratas <chloevision97@gmail.com>
2023-12-17 01:01:28 +0100

    Adding get all by user

--- src/routes/transactions/mod.rs
+++ src/routes/transactions/mod.rs
@@ -1,9 +1,12 @@
 pub mod create_transaction;
+pub mod get_all_transactions_by_user;
 
 use actix_web::web::ServiceConfig;
 
 pub fn transactions_routes_config(cfg: &mut ServiceConfig) {
     cfg.service(
-        actix_web::web::scope("/transactions").service(create_transaction::create_transaction),
+        actix_web::web::scope("/transactions")
+            .service(create_transaction::create_transaction)
+            .service(get_all_transactions_by_user::get_all_transactions_by_user),
     );
 }
