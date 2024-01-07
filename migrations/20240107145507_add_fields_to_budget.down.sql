8e478b283aa599115e16052cabb86ee8f41ae04e
Chloe Pratas <chloevision97@gmail.com>
2024-01-07 16:11:42 +0100

    writing sqlx migrations

--- migrations/20240107145507_add_fields_to_budget.down.sql
+++ migrations/20240107145507_add_fields_to_budget.down.sql
@@ -1,1 +1,3 @@
 -- Add down migration script here
+ALTER TABLE budgets DROP COLUMN IF EXISTS recurring;
+ALTER TABLE budgets DROP COLUMN IF EXISTS duration_unix;
--- migrations/20240107145507_add_fields_to_budget.up.sql
+++ migrations/20240107145507_add_fields_to_budget.up.sql
@@ -1,1 +1,3 @@
 -- Add up migration script here
+ALTER TABLE budgets ADD COLUMN IF NOT EXISTS recurring boolean NOT NULL DEFAULT false;
+ALTER TABLE budgets ADD COLUMN IF NOT EXISTS duration_unix int NOT NULL 
