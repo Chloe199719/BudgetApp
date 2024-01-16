8b60c2fe07887d542cc8a12b8ac4504be6e477c5
Chloe Pratas <chloevision97@gmail.com>
2024-01-17 00:19:08 +0100

    Another test

--- tests/api/budgets/change_budget_amount.rs
+++ tests/api/budgets/change_budget_amount.rs
@@ -96,3 +96,48 @@ async fn test_change_budget_amount(pool: PgPool) {
     );
     assert_eq!(login_response_body.id, get_category_response_body.user_id);
 }
+
+#[sqlx::test]
+async fn test_change_budget_amount_error(pool: PgPool) {
+    let app = spawn_app(pool.clone()).await;
+
+    //Act - Part 1 - Login
+    let login_body = LoginUser {
+        email: app.test_user.email.clone(),
+        password: app.test_user.password.clone(),
+    };
+    let login_response = app.post_login(&login_body).await;
+    assert!(login_response.status().is_success());
+
+    //Act - Part 2 - Create budget
+
+    let create_budget_body = CreateBudget {
+        amount: 100.0,
+        start_date: chrono::Utc::now(),
+        end_date: chrono::Utc::now() + chrono::Duration::days(30),
+        recurring: true,
+    };
+
+    let create_budget_response = app
+        .api_client
+        .post(&format!("{}/budgets/create/{}", app.address, 1))
+        .json(&create_budget_body)
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    assert!(create_budget_response.status().is_success());
+
+    //Act - Part 3 - Change budget amount
+    let change_budget_amount_body = ChangeBudgetAmount { amount: 200.0 };
+
+    let change_budget_amount_response = app
+        .api_client
+        .patch(&format!("{}/budgets/change_amount/{}", app.address, 100))
+        .json(&change_budget_amount_body)
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    assert!(change_budget_amount_response.status().is_client_error());
+}
