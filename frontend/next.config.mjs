f7a3f125250bfa9b756e140cd4b69b6a53040414
Chloe Pratas <chloevision97@gmail.com>
2024-01-18 01:33:43 +0100

    Update eslint and next.config

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,6 @@
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
   images: {
     remotePatterns: [
@@ -10,4 +12,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
