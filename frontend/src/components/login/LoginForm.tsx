dadccb012b051c32ddbd7107bc716f949f3714fb
Chloe Pratas <chloevision97@gmail.com>
2024-01-19 19:32:12 +0100

    Refactor login form component

--- frontend/src/components/login/LoginForm.tsx
+++ frontend/src/components/login/LoginForm.tsx
@@ -1,23 +1,47 @@
 "use client";
-
+import { zodResolver } from "@hookform/resolvers/zod";
 import axios from "axios";
 import { useState } from "react";
 import { CurrentUserData } from "../../app/layout";
 import { useDispatch } from "@/lib/redux/store";
 import { login } from "@/lib/redux/slices/auth";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+import { APP_NAME } from "@/lib/constants";
+const formSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(2),
+});
 
 type Props = {};
 function LoginForm({}: Props) {
-  const [email, setEmail] = useState("");
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
   const dispatch = useDispatch();
-  const [password, setPassword] = useState("");
-  async function onsubmit(e: React.FormEvent<HTMLFormElement>) {
-    e.preventDefault();
+
+  async function onsubmit(e: z.infer<typeof formSchema>) {
     const res = await axios.post(
       "http://localhost:5000/users/login/",
       {
-        email,
-        password,
+        email: e.email,
+        password: e.password,
       },
       { withCredentials: true }
     );
@@ -26,32 +50,52 @@ function LoginForm({}: Props) {
     dispatch(login({ ...data, isAuthenticated: true }));
   }
   return (
-    <div>
-      <form onSubmit={onsubmit}>
-        <label htmlFor="username">Username:</label>
-        <input
-          onChange={(e) => {
-            setEmail(e.currentTarget.value);
-          }}
-          value={email}
-          type="text"
-          id="username"
-          name="username"
-          className="text-black"
-        />
-        <label htmlFor="password">Password:</label>
-        <input
-          onChange={(e) => {
-            setPassword(e.currentTarget.value);
-          }}
-          value={password}
-          type="password"
-          id="password"
-          name="password"
-          className="text-black"
-        />
-        <button>Submit</button>
-      </form>
+    <div className="w-full col-span-2  px-8 h-full flex items-center justify-center flex-col gap-8">
+      <h2 className="text-4xl text-pretty font-bold text-black">
+        Sign in to your <span className="text-blue-500"> {APP_NAME} </span>{" "}
+        Account
+      </h2>
+      <div className="w-full flex flex-col">
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onsubmit)}
+            className="space-y-4 bg-blue-200 p-8 rounded-md"
+          >
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Chloe" {...field} />
+                  </FormControl>
+
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input type="password" placeholder="Password" {...field} />
+                  </FormControl>
+
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <Button className="w-full" type="submit">
+              Login
+            </Button>
+          </form>
+        </Form>
+      </div>
     </div>
   );
 }
