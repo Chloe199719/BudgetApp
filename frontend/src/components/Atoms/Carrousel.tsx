eefd3f20626ec64cf6b917f311ddd05804074149
Chloe Pratas <chloevision97@gmail.com>
2024-01-19 19:32:08 +0100

    Update Carrousel component styling

--- frontend/src/components/Atoms/Carrousel.tsx
+++ frontend/src/components/Atoms/Carrousel.tsx
@@ -1,5 +1,5 @@
 type Props = {};
 function Carrousel({}: Props) {
-  return <div>Carrosel</div>;
+  return <div className="bg-blue-300 w-full h-full col-span-4">Carrosel</div>;
 }
 export default Carrousel;
