a1212e27ebb7c2dce0724bf4ef8a2ad2d22e56b5
Chloe Pratas <chloevision97@gmail.com>
2024-01-19 19:31:56 +0100

    Add Carrousel component and redirect user if logged in

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,11 +1,23 @@
+import Carrousel from "@/components/Atoms/Carrousel";
 import LoginForm from "../../components/login/LoginForm";
 
+import { redirect } from "next/navigation";
+import { getUserData } from "../layout";
+import { cookies } from "next/headers";
+
 type Props = {};
-function page({}: Props) {
+async function page({}: Props) {
+  const cookieStore = cookies();
+  const sessionId = cookieStore.get("sessionid");
+  const user = await getUserData(sessionId?.value);
+  if (user) {
+    redirect("/");
+  }
   return (
-    <main className="flex flex-1 flex-col items-center justify-between ">
-      <section>
+    <main className="flex flex-1">
+      <section className="w-full grid grid-cols-6  justify-items-center">
         <LoginForm />
+        <Carrousel />
       </section>
     </main>
   );
