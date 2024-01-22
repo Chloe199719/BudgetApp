22ce969d573e4d2c0fc27e9ccfb9725b77d41301
Chloe Pratas <chloevision97@gmail.com>
2024-01-22 23:41:43 +0100

    Add sign-up page and sign-up form component

---
+++ frontend/src/app/sign-up/page.tsx
@@ -0,0 +1,26 @@
+import Carrousel from "@/components/Atoms/Carrousel";
+import LoginForm from "../../components/login/LoginForm";
+
+import { redirect } from "next/navigation";
+import { getUserData } from "../layout";
+import { cookies } from "next/headers";
+import SignUpForm from "@/components/sign-up/SignupForm";
+
+type Props = {};
+async function Page({}: Props) {
+  const cookieStore = cookies();
+  const sessionId = cookieStore.get("sessionid");
+  const user = await getUserData(sessionId?.value);
+  if (user) {
+    redirect("/");
+  }
+  return (
+    <main className="flex flex-1">
+      <section className="w-full grid grid-cols-1  md:grid-cols-6  justify-items-center p-10">
+        <SignUpForm />
+        <Carrousel />
+      </section>
+    </main>
+  );
+}
+export default Page;
