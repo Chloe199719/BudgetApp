fe92548bcf1145243b0c39b6e62df9e118e733e5
Chloe Pratas <chloevision97@gmail.com>
2024-01-22 23:56:17 +0100

    Refactor register function in register.ts

--- frontend/src/lib/api/auth/register.ts
+++ frontend/src/lib/api/auth/register.ts
@@ -1,16 +1,27 @@
-import { SignUpFormData } from "@/components/sign-up/SignupForm";
-import axiosInstance from "../axios";
+import { SignUpFormData } from '@/components/sign-up/SignupForm';
+import axiosInstance from '../axios';
+import { SuccessResponse } from '@/lib/types/sucesssResponse';
+import { AxiosError } from 'axios';
+import { ErrorResponse } from '@/lib/types/errorResponse';
 
-export async function register({
-  email,
-  password,
-  unique_name,
-  display_name,
-}: SignUpFormData) {
-  await axiosInstance.post("/register", {
+export async function registerUserApi({
     email,
     password,
     unique_name,
     display_name,
-  });
+}: SignUpFormData) {
+    try {
+        const res = await axiosInstance.post('/users/register', {
+            email,
+            password,
+            unique_name,
+            display_name,
+        });
+        return res.data as SuccessResponse;
+    } catch (error) {
+        if (error instanceof AxiosError) {
+            throw error.response?.data as ErrorResponse;
+        }
+        throw { error: 'Unknown error' };
+    }
 }
