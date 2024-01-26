7806518b2c01e88b397ce4993a5e749789da96cb
Chloe Pratas <chloevision97@gmail.com>
2024-01-26 23:52:34 +0100

    Add passwordsSlice to rootReducer

--- frontend/src/lib/redux/rootReducer.ts
+++ frontend/src/lib/redux/rootReducer.ts
@@ -1,5 +1,6 @@
 import authSlice from "./slices/auth";
-
+import passwordsSlice from "./slices/forgotPasswordSlice/passwords";
 export const reducer = {
     auth: authSlice,
+    passwords: passwordsSlice,
 };
