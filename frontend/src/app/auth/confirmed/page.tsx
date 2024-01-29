6310d786106e73d2aed69b9ed2cecc80a06c34e7
Chloe Pratas <chloevision97@gmail.com>
2024-01-29 20:20:12 +0100

    Update login message in confirmed page

--- frontend/src/app/auth/confirmed/page.tsx
+++ frontend/src/app/auth/confirmed/page.tsx
@@ -7,7 +7,7 @@ function page({}: Props) {
             <h2 className="text-4xl text-green-600">
                 Thank you for Confirming Your Email
             </h2>
-            <h3 className="text-2xl text-green-400">You now Login</h3>
+            <h3 className="text-2xl text-green-400">You can now Login</h3>
             <Link className="bg-blue-500 p-4 w-full" href="/auth/login">
                 Login
             </Link>
