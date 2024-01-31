9280c29d6d044ace25e0b647f19572ca3c27d0a5
Chloe Pratas <chloevision97@gmail.com>
2024-01-31 01:52:37 +0100

    Refactor authSlice in redux/slices/auth.ts

--- frontend/src/lib/redux/slices/auth.ts
+++ frontend/src/lib/redux/slices/auth.ts
@@ -2,48 +2,48 @@ import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 
 export type AuthState = NotLoggedIn | LoggedIn;
 type NotLoggedIn = {
-  isAuthenticated: false;
+    isAuthenticated: false;
 };
 type LoggedIn = {
-  isAuthenticated: true;
-  id: string;
-  email: string;
-  display_name: string;
-  unique_name: string;
-  is_active: boolean;
-  is_staff: boolean;
-  is_superuser: boolean;
-  thumbnail?: string;
-  data_joined: string;
-  profile: {
+    isAuthenticated: true;
     id: string;
-    phone_number?: string;
-    about_me?: string;
-    pronouns?: string;
-    avatar_link?: string;
-    birth_date?: string;
-    github_link?: string;
-  };
+    email: string;
+    display_name: string;
+    unique_name: string;
+    is_active: boolean;
+    is_staff: boolean;
+    is_superuser: boolean;
+    thumbnail?: string;
+    data_joined: string;
+    profile: {
+        id: string;
+        phone_number?: string;
+        about_me?: string;
+        pronouns?: string;
+        avatar_link?: string;
+        birth_date?: string;
+        github_link?: string;
+    };
 };
 
 const initialState: AuthState = {
-  isAuthenticated: false,
+    isAuthenticated: false,
 } as AuthState;
 
 export const authSlice = createSlice({
-  name: "auth",
-  initialState,
-  reducers: {
-    login: (state, action: PayloadAction<LoggedIn>) => {
-      state = { ...action.payload } as LoggedIn;
+    name: "auth",
+    initialState,
+    reducers: {
+        login: (state, action: PayloadAction<LoggedIn>) => {
+            state = { ...action.payload } as LoggedIn;
 
-      return state;
+            return state;
+        },
+        logout: (state) => {
+            state = initialState;
+            return state;
+        },
     },
-    logout: (state) => {
-      state = initialState;
-      return state;
-    },
-  },
 });
 
 export const { login, logout } = authSlice.actions;
