54287df7cb15f34153428c87159a1066db5e8278
Chloe Pratas <chloevision97@gmail.com>
2024-02-01 01:39:45 +0100

    Add LandingPage component to Home page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,10 @@
 import CallToAction from "@/components/landingPage/CallToAction";
-
+import LandingPage from "@/components/landingPage/LandingPage";
 export default function Home() {
-  return (
-    <main className="flex flex-1 flex-col items-center justify-between ">
-      <CallToAction />
-    </main>
-  );
+    return (
+        <main className="flex flex-1 flex-col items-center justify-between border-t border-white ">
+            {/* <CallToAction /> */}
+            <LandingPage />
+        </main>
+    );
 }
